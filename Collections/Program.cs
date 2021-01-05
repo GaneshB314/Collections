d1f9c59850a206251b1ea7f7b135d8608c583fb0
ganeshbalaji08111994 <ganeshbalaji08111994@gmail.com>
2021-01-05 21:07:10 +0530

    AddProgram

---
+++ Collections/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ArrayList al = new ArrayList(5);
+            Console.WriteLine(" The Capacity of ArrayList is " + al.Capacity);
+            al.Add(100);
+            al.Add("Ganesh");
+            al.Add(53.5);
+            al.Add('B');
+            al.Add(52.354);
+            al.Add(52);
+
+            foreach (var item in al)
+            {
+                Console.WriteLine("The Items are" + item);
+             
+
+            }
+            Console.WriteLine(" The Capacity of ArrayList after Increasing is " + al.Capacity);
+            al.RemoveAt(0);
+            Console.WriteLine(" The Capacity of ArrayList after Decreasing is " + al.Capacity);
+            al.Add(100);
+            al.Add("Ganesh");
+            al.Add(53.5);
+            al.Add('B');
+            al.Add(52.354);
+            al.Add(52);
+            Console.WriteLine(" The Capacity of ArrayList after Increasing next 10 is " + al.Capacity);
+            Console.Read();
+
+
+        }
+    }
+}
